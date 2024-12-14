a62851f79cd3585102b6bdb47c4309f192e7dd64
Mateusz Kowalski <towermanxxl@gmail.com>
2024-12-15 00:33:31 +0100

    added category to songs table

--- db/Migrations/migrateSongs.js
+++ db/Migrations/migrateSongs.js
@@ -1,6 +1,6 @@
 function MigrateSongs(db) {
     
-    db.run("CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY,title TEXT, lyrics TEXT)")
+    db.run("CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY,title TEXT, lyrics TEXT, category TEXT)")
        
     
 
--- db/utils/FillDatabase.js
+++ db/utils/FillDatabase.js
@@ -4,14 +4,14 @@ async function FillWithSampleData(db){
     const fs = require("fs")
     const path = require("path")
     
-    const fill = db.prepare("INSERT INTO songs(title,lyrics) VALUES (?,?)")
+    const fill = db.prepare("INSERT INTO songs(title,lyrics,category) VALUES (?,?,?)")
     const check = db.prepare("SELECT * FROM songs WHERE title =? ")
 
     const filepath = path.join(__dirname,"data.json")
     var data = fs.readFileSync(filepath,'utf-8')
     data = JSON.parse(data) 
   
-    
+    const category = data["Category"]
     for (var sample of data["SampleDataSongs"]){
         await new Promise((resolve, reject) => {
             check.get(sample[0], (err, row) => {
@@ -21,7 +21,7 @@ async function FillWithSampleData(db){
                 } else {
                     if (!row) {
                         console.log(row)
-                        fill.run(sample[0], JSON.stringify(sample[1]), (err) => {
+                        fill.run(sample[0], JSON.stringify(sample[1]), category, (err) => {
                             if (err) {
                                 console.error("error while inserting");
                                 reject(err);
