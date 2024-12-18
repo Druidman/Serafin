75216ea4dba31a478842cb16b6c5800fbc86a266
Mateusz Kowalski <towermanxxl@gmail.com>
2024-12-18 19:31:41 +0100

    loading previews as default

--- app/renderer/renderer.js
+++ app/renderer/renderer.js
@@ -2,12 +2,12 @@
 import "./dom/eventHandlers.js"
 
 // module imports
-import { load_categories } from "./dom/dbutils.js"
-import { getSongCategories } from "./dom/ipcHandlers.js"
+import { load_categories, load_previews } from "./dom/dbutils.js"
+import { getSongCategories, getSongsPreview } from "./dom/ipcHandlers.js"
 import { openNewWindow } from "./dom/windowManager/window.js"
 
-var categories = getSongCategories()
-load_categories(categories)
+var previews = getSongsPreview(100,"wszystko")
+load_previews(previews)
 
 openNewWindow("display.html","displayWind")
 
