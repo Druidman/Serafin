eb0abe22b56a2f93a1486afaf3514cdae5a43e0a
Mateusz Kowalski <104452414+Druidman@users.noreply.github.com>
2025-01-24 14:50:14 +0100

    remove windowMenuBar

--- app/main/windowManager/mainWindow.js
+++ app/main/windowManager/mainWindow.js
@@ -8,6 +8,7 @@ function createMainWindow(){
   const win = new BrowserWindow({
     width: 1280,
     height: 720,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, '../preloads/mainWindowPreload.js')
     }
