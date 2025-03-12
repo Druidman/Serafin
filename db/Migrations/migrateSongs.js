6d7ab97008e011c78025e89eb1d6241a06167e87
Mateusz Kowalski <towermanxxl@gmail.com>
2025-03-12 21:18:05 +0100

    changed table layout and added new .json data file

--- db/Migrations/migrateSongs.js
+++ db/Migrations/migrateSongs.js
@@ -1,6 +1,12 @@
 function MigrateSongs(db) {
     
-    db.run("CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY,title TEXT, lyrics TEXT, category TEXT)")
+    db.run("CREATE TABLE IF NOT EXISTS songs (\
+        id INTEGER PRIMARY KEY,\
+        title TEXT,\
+        category TEXT\
+        chorus TEXT,\
+        lyrics TEXT\
+    )")
        
     
 
